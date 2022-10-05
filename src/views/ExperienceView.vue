6a995da5d899a0349484744ae6b6ed954ea74213
Philani Mxathule <3911755@myuwc.ac.za>
2022-10-05 15:54:18 +0200

    full-content

--- src/views/ExperienceView.vue
+++ src/views/ExperienceView.vue
@@ -2,10 +2,10 @@
     <div id="experience" class="container text-start my-5 h-100 justify-content-between overflow-auto">
       <h2 class="text-center bg-black my-5 p-3 fw-bolder">Work Experience</h2>
       <div class="row">
-        <div class="col-md-12">
+        <div class="col-md-6">
           <div class="">
             <div class="text-black">
-              <div>
+              <div class="item">
                 <h4 class="text-black">PAY intern @Western Cape Government</h4>
                 <h6 class="text-black">Cultural Affairs and Sport</h6>
                 <h6>SOMS: [CRU] Corporate Relations Unit </h6>
@@ -13,7 +13,7 @@
                   Leave administration PAY interns payslip processing Overtime, Performance Management </p>
               </div>
               <hr>
-              <div>
+              <div class="item">
                 <h4 class="text-black">Tradeway</h4>
                 <h6 class="text-black">Marketing and Sales Assistant</h6>
                 <h6></h6>
@@ -23,8 +23,8 @@
             <hr>
           </div>
         </div>
-        <div class="col-md-12">
-          <div class="text-black">
+        <div class="col-md-6">
+          <div class="text-black item">
             <h4 class="text-black">British Airways Tobacco</h4>
             <h6 class="text-black">Promoter</h6>
             <h6></h6>
@@ -33,7 +33,7 @@
           </div>
           <hr>
           <div class="my-3 text-black">
-            <div>
+            <div class="item">
               <h4 class="text-black">Lifechoices Studio</h4>
               <h6 class="text-black">Web developer Intern/Student</h6>
               <p>Duties: Creating website based projects using programming languages that we are learning at the academy.
@@ -55,6 +55,14 @@
   </script>
   
   <style scoped>
+.item {
+  width: 500px;
+  height: 200px;
+  background: rgba(141, 141, 141, 0.7);
+  text-align: center;
+  color: #4E4C48;
+  font-size: 1.1em;
+}
   .row {
     background: #ffffff;
     color: #898989;
