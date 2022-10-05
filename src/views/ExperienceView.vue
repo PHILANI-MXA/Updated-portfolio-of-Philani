f324146c8bee8ffecbf3ecd3dbb01aedcacb14a4
Philani Mxathule <3911755@myuwc.ac.za>
2022-10-05 16:00:46 +0200

    full-content

--- src/views/ExperienceView.vue
+++ src/views/ExperienceView.vue
@@ -2,10 +2,10 @@
     <div id="experience" class="container text-start my-5 h-100 justify-content-between overflow-auto">
       <h2 class="text-center bg-black my-5 p-3 fw-bolder">Work Experience</h2>
       <div class="row">
-        <div class="col-md-6">
+        <div class="col-md-6" >
           <div class="">
             <div class="text-black">
-              <div class="item">
+              <div class="item" data-aos="fade-right" data-aos-anchor="#example-anchor" data-aos-offset="500" data-aos-duration="500">
                 <h4 class="text-black">PAY intern @Western Cape Government</h4>
                 <h6 class="text-black">Cultural Affairs and Sport</h6>
                 <h6>SOMS: [CRU] Corporate Relations Unit </h6>
@@ -13,7 +13,7 @@
                   Leave administration PAY interns payslip processing Overtime, Performance Management </p>
               </div>
               <hr>
-              <div class="item">
+              <div class="item" data-aos="fade-right" data-aos-anchor="#example-anchor" data-aos-offset="500" data-aos-duration="500">
                 <h4 class="text-black">Tradeway</h4>
                 <h6 class="text-black">Marketing and Sales Assistant</h6>
                 <h6></h6>
@@ -24,7 +24,7 @@
           </div>
         </div>
         <div class="col-md-6">
-          <div class="text-black item">
+          <div class="text-black item" data-aos="fade-left" data-aos-anchor="#example-anchor" data-aos-offset="500" data-aos-duration="500">
             <h4 class="text-black">British Airways Tobacco</h4>
             <h6 class="text-black">Promoter</h6>
             <h6></h6>
@@ -33,7 +33,7 @@
           </div>
           <hr>
           <div class="my-3 text-black">
-            <div class="item">
+            <div class="item" data-aos="fade-left" data-aos-anchor="#example-anchor" data-aos-offset="500" data-aos-duration="500">
               <h4 class="text-black">Lifechoices Studio</h4>
               <h6 class="text-black">Web developer Intern/Student</h6>
               <p>Duties: Creating website based projects using programming languages that we are learning at the academy.
@@ -50,15 +50,23 @@
   
   <script>
   export default {
-    name: ''
+    name: '',
+    components: {
+    },
+    mounted() {
+        AOS.init()
+    }
   }
+AOS.init({
+    duration: 1000
+});
   </script>
   
   <style scoped>
 .item {
   width: 500px;
   height: 200px;
-  background: rgba(141, 141, 141, 0.7);
+  background: #f6f6f6;
   text-align: center;
   color: #4E4C48;
   font-size: 1.1em;
