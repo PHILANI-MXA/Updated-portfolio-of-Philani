2c09adb58938d504f8a26e9ff03539b88931c577
Philani Mxathule <3911755@myuwc.ac.za>
2022-11-12 23:44:34 +0200

    commit

--- src/views/Testimonial22.vue
+++ src/views/Testimonial22.vue
@@ -1,9 +1,9 @@
 <template>
-    <div id="testimonials" class="container h-100 p-5 my-5 m-auto justify-content-between">
+  <!-- <div id="testimonials" class="container h-100 p-5 my-5 m-auto justify-content-between">
         <h2 class="text-center bg-black my-5 p-3 fw-bolder text-white">Testimonials</h2>
         <div class="row m-auto align-items-center d-flex">
             <div class="col-md-2"><hr></div>
-            <div class="col-md-9 justify-content-between px-4 m-auto">
+            <div class="col-md-9 justify-content-between m-auto">
             <div class="m-auto">
                 <figure>
                     <img src="https://i.postimg.cc/g0pyf3Cg/Silizwe-2.jpg" alt="Portfolio Item">
@@ -59,88 +59,350 @@
         </div>
         <div class="col-md-1"><hr></div>
     </div>
+    </div> -->
+  <div
+    id="education"
+    class="container text-start my-5 p-4 h-100 justify-content-between"
+  >
+    <h2 class="text-center bg-black my-5 p-3 fw-bolder">Testimonials</h2>
+    <div class="row d-inline-flex">
+      <!-- <div class="row">
+          <div class="col-md-6" >
+            <div class="">
+              <div class="text-black">
+                <div class="item" data-aos="fade-right">
+                  <div>
+                    <h4 class="text-black">Tradeway</h4>
+                    <h6 class="text-black">Marketing and Sales Assistant</h6>
+                    <h6></h6>
+                    <p>Store promoter, promoting various items for different retail companies.</p>
+                    </div>
+                </div>
+                <hr>
+                <div class="item" data-aos="fade-right">
+                  <div class="text-black item">
+                              <h4 class="text-black">PAY intern @Western Cape Government</h4>
+                              <h6 class="text-black">Cultural Affairs and Sport</h6>
+                              <h6>SOMS: [CRU] Corporate Relations Unit </h6>
+                              <p>Duties: Human Resource Admin Support Working with Persal system under the following fields Housing
+                                Leave administration PAY interns payslip processing Overtime, Performance Management </p>
+                          </div>
+                </div>
+              </div>
+              <hr>
+            </div>
+          </div>
+          <div class="col-md-6">
+            <div class="text-black item" data-aos="fade-left">
+              <div class="">
+                <h4 class="text-black">British Airways Tobacco</h4>
+                <h6 class="text-black">Promoter</h6>
+                <h6></h6>
+                <p>Promoting ciggarets for retail stores, influencing customers to buy ciggarets that are associated with BAT
+                </p>
+                  </div>
+            </div>
+            <hr>
+            <div class="my-3 text-black">
+              <div class="item" data-aos="fade-left">
+                  <div class="my-3 text-black">
+                    <h4 class="text-black">Lifechoices Studio</h4>
+                    <h6 class="text-black">Web developer Intern/Student</h6>
+                    <p>Duties: Creating website based projects using programming languages that we are learning at the academy.
+                      Place for upskilling myself. Attending professional development trainigs and boosting my soft skills</p>
+                  </div>
+              </div>
+            </div>
+          </div>
+        </div> -->
+
+      <div class="col-md-6 item" data-aos="fade-right">
+        <figure>
+          <img
+            src="https://i.postimg.cc/kXryQrjr/Sibongile-2.jpg"
+            alt="Portfolio Item"
+          />
+          <figcaption>
+            <h6>Sibongile</h6>
+            <p>
+              Philani is open minded developer he takes his time whenever he
+              encounters a problem he is always cool, calm and collected.
+            </p>
+          </figcaption>
+        </figure>
+      </div>
+      <div class="item col-md-6" data-aos="fade-right">
+        <figure>
+          <img
+            src="https://i.postimg.cc/Fz6JLctV/image-6487327.jpg"
+            alt="Portfolio Item"
+          />
+          <figcaption>
+            <h6>Rasheedah</h6>
+            <p>
+              Philani is very hardworking and possesses a keen attention to
+              detail. He is always ready and willing to assist his peers.
+            </p>
+          </figcaption>
+        </figure>
+      </div>
     </div>
+    <div class="row d-inline-flex">
+      <div class="col-md-6 item" data-aos="fade-right">
+        <figure>
+          <img
+            src="https://i.postimg.cc/rshkbdbJ/9-BF3-E51-D-57-C5-49-C6-BA88-AE494436-B96-B.jpg"
+            alt="Portfolio Item"
+          />
+          <figcaption>
+            <h6>Lunga</h6>
+            <p>
+              Philani is a problem solver he does that with ease and peace of
+              mind we work well together
+            </p>
+          </figcaption>
+        </figure>
+      </div>
+      <div class="item col-md-6" data-aos="fade-right">
+        <figure>
+          <img
+            src="https://i.postimg.cc/g0pyf3Cg/Silizwe-2.jpg"
+            alt="Portfolio Item"
+          />
+          <figcaption>
+            <h6>Siliziwe</h6>
+            <p>
+              Philani is a promising and open minded person. He is a multi
+              skilled developer and I work well with him.
+            </p>
+          </figcaption>
+        </figure>
+      </div>
+    </div>
+    <div class="row row d-inline-flex">
+      <div class="col-md-6 item" data-aos="fade-right">
+        <figure>
+          <img
+            src="https://i.postimg.cc/DZLSFN1s/Godwin22.jpg"
+            alt="Portfolio Item"
+          />
+          <figcaption>
+            <h6>Godwin</h6>
+            <p>
+              Philani is very passionate about coding now I can assure that he
+              understands the code very well
+            </p>
+          </figcaption>
+        </figure>
+      </div>
+      <div class="item col-md-6" data-aos="fade-right">
+        <figure>
+          <img
+            src="https://i.postimg.cc/1Xqq4J2C/joel2.jpg"
+            alt="Portfolio Item"
+          />
+          <figcaption>
+            <h6>Joel</h6>
+            <p>
+              I hope that Philani can meet your expectations, he is so humble
+              and goal orientated.
+            </p>
+          </figcaption>
+        </figure>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import Testimonial22Vue from './Testimonial22.vue';
+import Testimonial22Vue from "./Testimonial22.vue";
 export default {
-    name: '',
-    components: {
-        Testimonial22Vue
-    },
-    mounted() {
-        AOS.init()
-    }
-}
+  name: "",
+  components: {
+    Testimonial22Vue,
+  },
+  mounted() {
+    AOS.init();
+  },
+};
 AOS.init({
-    duration: 1000
+  duration: 1000,
 });
 </script>
-  
+
 <style scoped>
-@import url('https://fonts.googleapis.com/css?family=Raleway:400,700');
+@import url("https://fonts.googleapis.com/css?family=Raleway:400,700");
 
 * {
-    font-family: Raleway, sans-serif;
-    font-weight: bold;
+  font-family: Raleway, sans-serif;
+  font-weight: bold;
 }
 
-body {
-    background-color: #f6f6f6;
-    background-image: -webkit-linear-gradient(left, hsla(0, 0%, 0%, 0) 0%, hsla(0, 0%, 0%, 0) 50%,
-            hsla(0, 0%, 0%, .015) 50%, hsla(0, 0%, 0%, .015) 100%),
-        -webkit-radial-gradient(hsla(0, 0%, 0%, .05) 10%, transparent 15%);
-    background-size: 1.5em 1.5em, .5em .5em;
-    font-family: sans-serif;
-    font-size: 1.1em;
-    line-height: 1.5;
+template {
     margin: auto;
 }
 
+body {
+  background-color: #f6f6f6;
+  background-image: -webkit-linear-gradient(
+      left,
+      hsla(0, 0%, 0%, 0) 0%,
+      hsla(0, 0%, 0%, 0) 50%,
+      hsla(0, 0%, 0%, 0.015) 50%,
+      hsla(0, 0%, 0%, 0.015) 100%
+    ),
+    -webkit-radial-gradient(hsla(0, 0%, 0%, 0.05) 10%, transparent 15%);
+  background-size: 1.5em 1.5em, 0.5em 0.5em;
+  font-family: sans-serif;
+  font-size: 1.1em;
+  line-height: 1.5;
+  margin: auto;
+}
+
 figure {
-    float: left;
-    height: 15em;
-    margin: au;
-    width: 18em;
-    -webkit-transform: perspective(500); 
-    -webkit-transform-style: preserve-3d;
-    -webkit-transition: .5s;
+  float: left;
+  height: 15em;
+  margin: au;
+  width: 18em;
+  -webkit-transform: perspective(500);
+  -webkit-transform-style: preserve-3d;
+  -webkit-transition: 0.5s;
 }
 
 .figcaption {
-    height: 15em;
+  height: 15em;
 }
 
 figure:hover {
-    -webkit-transform: perspective(500) rotateX(90deg) translateY(-3em) translateZ(3em);
+  -webkit-transform: perspective(500) rotateX(90deg) translateY(-3em)
+    translateZ(3em);
 }
 
 img {
-    background-color: #222;
-    box-shadow: 0 20px 15px -10px hsla(0, 0%, 0%, .25);
-    display: block;
-    height: 100%;
-    -webkit-transition: .5s;
-    width: 100%;
-    object-fit: cover;
-    object-position: 0 0;
+  background-color: #222;
+  box-shadow: 0 20px 15px -10px hsla(0, 0%, 0%, 0.25);
+  display: block;
+  height: 100%;
+  -webkit-transition: 0.5s;
+  width: 100%;
+  object-fit: cover;
+  object-position: 0 0;
 }
 
 figure:hover img {
-    box-shadow: none;
+  box-shadow: none;
 }
 
 figcaption {
-    background-color: #222;
-    color: #fff;
-    padding: 1.5em;
-    -webkit-transform: rotateX(-90deg);
-    -webkit-transform-origin: 100% 0;
-    -webkit-transition: .5s;
+  background-color: #222;
+  color: #fff;
+  padding: 1.5em;
+  -webkit-transform: rotateX(-90deg);
+  -webkit-transform-origin: 100% 0;
+  -webkit-transition: 0.5s;
 }
 
 figure:hover figcaption {
-    box-shadow: 0 20px 15px -10px hsla(0, 0%, 0%, .25);
+  box-shadow: 0 20px 15px -10px hsla(0, 0%, 0%, 0.25);
 }
+.row {
+  background: #ffffff;
+  color: #898989;
+  font-family: "Poppins", helvetica;
+  font-size: 15px;
+  font-weight: 300;
+  line-height: 20px;
+  letter-spacing: 0.02em;
+  margin: 0 auto;
+  padding: 0;
+  -moz-box-sizing: border-box;
+  -ms-box-sizing: border-box;
+  -o-box-sizing: border-box;
+  -webkit-box-sizing: border-box;
+  box-sizing: border-box;
+}
+
+img {
+  border-radius: 10px;
+  box-shadow: solid grey;
+}
+
+.item {
+  width: 50%;
+  height: 345px;
+  background: #e8eeef;
+  text-align: center;
+  color: #4e4c48;
+  font-size: 1.1em;
+  flex-wrap: wrap;
+}
+
+/* {
+  font-family: Raleway, sans-serif;
+  font-weight: bold;
+}
+
+body {
+  background-color: #f6f6f6;
+  background-image: -webkit-linear-gradient(
+      left,
+      hsla(0, 0%, 0%, 0) 0%,
+      hsla(0, 0%, 0%, 0) 50%,
+      hsla(0, 0%, 0%, 0.015) 50%,
+      hsla(0, 0%, 0%, 0.015) 100%
+    ),
+    -webkit-radial-gradient(hsla(0, 0%, 0%, 0.05) 10%, transparent 15%);
+  background-size: 1.5em 1.5em, 0.5em 0.5em;
+  font-family: sans-serif;
+  font-size: 1.1em;
+  line-height: 1.5;
+  margin: auto;
+}
+
+figure {
+  float: left;
+  height: 15em;
+  margin: au;
+  width: 18em;
+  -webkit-transform: perspective(500);
+  -webkit-transform-style: preserve-3d;
+  -webkit-transition: 0.5s;
+}
+
+.figcaption {
+  height: 15em;
+}
+
+figure:hover {
+  -webkit-transform: perspective(500) rotateX(90deg) translateY(-3em)
+    translateZ(3em);
+}
+
+img {
+  background-color: #222;
+  box-shadow: 0 20px 15px -10px hsla(0, 0%, 0%, 0.25);
+  display: block;
+  height: 100%;
+  -webkit-transition: 0.5s;
+  width: 100%;
+  object-fit: cover;
+  object-position: 0 0;
+}
+
+figure:hover img {
+  box-shadow: none;
+}
+
+figcaption {
+  background-color: #222;
+  color: #fff;
+  padding: 1.5em;
+  -webkit-transform: rotateX(-90deg);
+  -webkit-transform-origin: 100% 0;
+  -webkit-transition: 0.5s;
+}
+
+figure:hover figcaption {
+  box-shadow: 0 20px 15px -10px hsla(0, 0%, 0%, 0.25);
+}*/
 </style>
