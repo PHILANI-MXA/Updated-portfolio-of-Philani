a3bcc0f9d875383fe19306f0a5a01d8e8ee7a9a5
Philani Mxathule <3911755@myuwc.ac.za>
2022-11-24 10:19:38 +0200

    commit

--- src/components/FooterComp.vue
+++ src/components/FooterComp.vue
@@ -3,28 +3,28 @@
     <footer class="text-center">
        <small class="text-black">Made by Philani Mxathule copyrights &copy; 2022</small>
        <div class="icons d-flex justify-content-between">
-           <a href="#home" target="_blank">
+           <a href="home" >
                  <i class="bi bi-house"></i>
                </a>
-           <a href="#about" target="_blank">
+           <a href="about" >
                  <i class="bi bi-file-person"></i>
                </a>
-           <a href="#education" target="_blank">
+           <a href="education" >
                  <i class="bi bi-mortarboard-fill"></i>
                </a>
-                <a href="#projects" target="_blank">
+                <a href="projects" >
                 <i class="bi bi-briefcase"></i>
                </a>
-           <a href="#skills" target="_blank">
+           <a href="skills" >
                  <i class="bi bi-code-slash"></i>
                </a>
-           <a href="certificates" target="_blank">
+           <a href="certificates" >
                  <i class="bi bi-bag-x"></i>
                </a>
-                <a href="#testimonials" target="_blank">
+                <a href="testimonials" >
                  <i class="bi bi-person-rolodex"></i>
                </a>
-                 <a href="#contact" target="_blank">
+                 <a href="contact" >
                    <i class="bi bi-telephone"></i>
                </a>
              
