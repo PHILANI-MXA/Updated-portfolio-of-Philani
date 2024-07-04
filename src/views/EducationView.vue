a17d9c9b88fde34dd35ee76ab7a6ba3aaa8f9c1c
Philani <mxathule99@gmail.com>
2024-07-05 01:30:43 +0200

    push

--- src/views/EducationView.vue
+++ src/views/EducationView.vue
@@ -5,44 +5,46 @@
         <div class="col-md-5 col-12 work_experience">
           <h2 class="experiencies p-4">Education</h2>
           <ol class="work">
-            <li class="we_item">
+            <li class="we_item shadow-sm" data-aos="fade-down" data-aos-duration="3000"
+            >
               <span class="carrec"><span class="li2019">[UWC]</span>University of the Western Cape</span>
               <span class="empresa">
                 <p lang="la">Bachelor of Library and Information Science [NQF L8] <br><span class="view"><a href="http://">view</a></span></p>
               </span>
               
             </li>
-            <li class="we_item">
+            <li class="we_item shadow-sm" data-aos="fade-up" data-aos-duration="3000">
               <span class="carrec"><span class="li2018">[LC]</span><abbr title="User experience"></abbr> Lifechoices Coding Academy</span>
               <p lang="la">Fullstack Development Bootcamp 6 Months <span class="view"><br><a href="http://">view</a></span></p>
               
             </li>
-            <li class="we_item">
+            <li class="we_item shadow-sm" data-aos="fade-down" data-aos-duration="3000"
+            >
               <span class="carrec"><span class="li2016">SL</span><abbr title="User Interface"></abbr> SoloLearn Coding</span>
               <p lang="la">Self taught - multi programming languages <br><span class="view"><a href="http://">view</a></span></p>
               
             </li>
-            <li class="we_item">
+            <li class="we_item shadow-sm" data-aos="fade-up" data-aos-duration="3000">
               <span class="carrec"><span class="li2016">UWC</span><abbr title="User Interface"></abbr>CoLab RPA Learnership </span>
               <p lang="la">Robotic Process Automation course <br><span class="view"><a href="http://">view</a></span></p>
               
             </li>
           </ol>
         </div>
-        <div class="col-md-7 col-12 education">
+        <div class="col-md-7 col-12 education" data-aos="zoom-out-down">
           <h2 class="education-title p-4">Experience</h2>
           <ol class="educa text-start ">
-            <li class="education_item px-5 ">
+            <li class="education_item px-5 shadow-sm" data-aos="flip-right">
               <span class="carrec"><span class="li2017"> LC </span> Studio 2022</span><br>
               <span class="empresa">Junior Fullstack Developer</span>
               <p>Capstone</p><p>Research analysis</p>
             </li>
-            <li class="education_item px-5">
+            <li class="education_item px-5 shadow-sm" data-aos="flip-left">
               <span class="carrec"><span class="li2017"> UWC </span> Library Services</span><br>
               <span class="empresa text-start">Work Integrated Learning</span>
               <p>Library Systems</p>
             </li>
-            <li class="education_item px-5">
+            <li class="education_item px-5 shadow-sm" data-aos="flip-right">
               <span class="carrec"><span class="li2017"> UWC </span>WC CoLab</span><br>
               <span class="empresa">RPA developer intern</span>
               <p>Power Platforms</p><p></p>
