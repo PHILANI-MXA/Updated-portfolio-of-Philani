5388cc209a686993b87fcb1384e03d3e4dd184b0
Philani <mxathule99@gmail.com>
2024-08-20 12:07:02 +0200

    projects-layout responsive

--- src/views/projects.vue
+++ src/views/projects.vue
@@ -4,34 +4,30 @@
     <router-link to="/skills">
       <div class="backbutton">
         <h1>Back</h1>
-      </div></router-link
-    >
-    <div class="row justify-content-between gy-5">
-      <div v-for="card in projects" class="card">
-        <div class="card-body">
-          <h5 class="card-title">{{ card.ProName }}</h5>
-          <h6 class="card-subtitle mb-2 text-muted">{{ card.ProTechLang }}</h6>
-          <img class="img-fluid" :src="card.Proimg" alt="" />
-          <a
-            :href="card.github"
-            class="yes btn btn-primaryd-flex btn-success flex-start"
-            >Ghub</a
-          >
-          <a
-            :href="card.live"
-            class="yes btn btn-primaryd-flex btn-danger flex-end"
-            >live</a
-          >
+      </div>
+    </router-link>
+    <div class="row justify-content-center gy-5">
+      <div v-for="card in projects" :key="card.id" class="col-12 col-sm-6 col-lg-4">
+        <div class="card h-100">
+          <div class="card-body d-flex flex-column">
+            <h5 class="card-title">{{ card.ProName }}</h5>
+            <h6 class="card-subtitle mb-2 text-muted">{{ card.ProTechLang }}</h6>
+            <img class="img-fluid mb-3" :src="card.Proimg" alt="Project Image" />
+            <div class="mt-auto d-flex justify-content-between">
+              <a :href="card.github" class="btn btn-success">Ghub</a>
+              <a :href="card.live" class="btn btn-danger">Live</a>
+            </div>
+          </div>
         </div>
       </div>
-      <h2 class="text-center bg-black my-5 p-3 fw-bolder text-white">
-        Tech Certificates
-      </h2>
-      <div v-for="pic in certificates" class="cards">
-        <div class="card-body">
-          <h5 class="card-title"></h5>
-          <h6 class="card-subtitle mb-2 text-muted"></h6>
-          <img class="img-fluid" :src="pic.img" alt="" />
+    </div>
+    <h2 class="text-center bg-black my-5 p-3 fw-bolder text-white">Tech Certificates</h2>
+    <div class="row justify-content-center gy-5">
+      <div v-for="pic in certificates" :key="pic.id" class="col-12 col-sm-6 col-lg-4">
+        <div class="card h-100">
+          <div class="card-body d-flex flex-column">
+            <img class="img-fluid" :src="pic.img" alt="Certificate Image" />
+          </div>
         </div>
       </div>
     </div>
@@ -52,49 +48,27 @@ export default {
 </script>
 
 <style scoped>
-.yes {
-  justify-content: space-around;
-}
-
 .card {
-  width: 40%;
-  height: 100%;
   background: #e8eeef;
   text-align: center;
   color: #4e4c48;
-  font-size: 1.1em;
-  line-height: 100px;
 }
 
-.cards {
-  width: 40%;
-  height: 100%;
-  background: #e8eeef;
-  text-align: center;
-  color: #4e4c48;
-  font-size: 1.1em;
-  line-height: 100px;
+.backbutton h1 {
+  font-size: 1.5rem;
+  color: #000;
 }
 
-/* .card {
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
-    flex-wrap: wrap;
-    color: black;
-    font-family: San-Francisco;
-    transition: 0.4s ease-out;
-    margin-bottom: 10px;
-    width: 40%;
-    height: 200px;
-} */
+img {
+  max-width: 100%;
+  height: auto;
+}
 
-.card:hover {
-  transform: translate3d(0px, -1px, 0px);
+.yes {
+  flex: 1;
 }
 
 .btn {
-  justify-content: space-between;
-  align-content: space-around;
+  width: 45%;
 }
 </style>
